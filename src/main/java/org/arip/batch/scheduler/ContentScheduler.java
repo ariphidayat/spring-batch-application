97b9e4c4d004a4824e149d696034069cfdcdcbd9
Arip Hidayat <ariphidayat@hotmail.com>
2018-01-05 09:35:32 +0700

    Spring Batch invoked by Spring Scheduler

--- src/main/java/org/arip/batch/scheduler/ContentScheduler.java
+++ src/main/java/org/arip/batch/scheduler/ContentScheduler.java
@@ -1,37 +1,35 @@
-package org.arip.batch.controller;
+package org.arip.batch.scheduler;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 /**
- * Created by Arip Hidayat on 1/4/2018.
+ * Created by Arip Hidayat on 1/5/2018.
  */
-@RestController
-public class ContentRestController {
-
-    @Autowired
-    private JobLauncher jobLauncher;
+@Component
+public class ContentScheduler {
 
     @Autowired
     private Job processJob;
 
-    @GetMapping("/startJob")
-    public String startJob() {
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Scheduled(fixedRate = 5000) //run every 5 sec
+    public void printNames() {
         try {
             JobParameters jobParameters = new JobParametersBuilder()
-                .addLong("time", System.currentTimeMillis())
-                .toJobParameters();
+                    .addLong("time", System.currentTimeMillis())
+                    .toJobParameters();
 
             jobLauncher.run(processJob, jobParameters);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        return "Batch job has been invoked";
     }
 }
